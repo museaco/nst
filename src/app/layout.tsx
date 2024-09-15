ecbf031de37c9ee4badf17e8f9f2cccdd8141d2d
Vision <lusheng10v@163.com>
2024-09-15 21:54:21 +0800

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import { ThemeProvider } from 'next-themes';
+import '@/styles/globals.css';
+
+const NotoSansSC = localFont({
+  src: '../fonts/NotoSansSC-Regular.ttf',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+
+
+export const metadata: Metadata = {
+  title: 'Ercy',
+  description: 'Ercy 动漫 二次元',
+};
+
+export default function RootLayout({ children, }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+    <body
+      className={`${NotoSansSC.variable} antialiased`}
+    >
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      disableTransitionOnChange
+      enableSystem={false}
+      enableColorScheme={false}
+    >
+      <div className="min-h-dvh  bg-background font-[family-name:var(--font-noto-sans)]">
+        {children}
+      </div>
+    </ThemeProvider>
+
+    </body>
+    </html>
+  );
+}
